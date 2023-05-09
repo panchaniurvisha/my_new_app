cf3ee22ccfdfecf7b68f140d8b7e1a1d0ceab6f3
Panchani Urvihsa <urvisha1109@gmail.com>
2023-05-09 11:17:44 +0530

    "9/5/2023"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/model_screen.dart';
+import 'package:my_app/tab_bar_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +15,6 @@ class MyApp extends StatelessWidget {
             //fontFamily: ''Dancing Script',
             primarySwatch: Colors.blue),
         debugShowCheckedModeBanner: false,
-        home: const ModelScreen());
+        home: const TabBarScreen());
   }
 }

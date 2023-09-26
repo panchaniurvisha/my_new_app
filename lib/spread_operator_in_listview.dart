1cc0549d6f5771d81db324af5d13b852043db5be
pills <urvisha1109@gmail.com>
2023-09-26 15:14:54 +0530

    "26/9/2023"

--- lib/spread_operator_in_listview.dart
+++ lib/spread_operator_in_listview.dart
@@ -12,26 +12,50 @@ class _SpreadOperatorInListViewState extends State<SpreadOperatorInListView> {
   @override
   Widget build(BuildContext context) {
     List<Widget> myList1 = [
-      const ListTile(title: Text('Item 1')),
-      const ListTile(title: Text('Item 2')),
-      const ListTile(title: Text('Item 3')),
+      SizedBox(
+        width: MediaQuery.of(context).size.width / 5,
+        child: const ListTile(
+          title: Text('Item 1'),
+        ),
+      ),
+      SizedBox(
+          width: MediaQuery.of(context).size.width / 5,
+          child: const ListTile(title: Text('Item 2'))),
+      SizedBox(
+          width: MediaQuery.of(context).size.width / 5,
+          child: const ListTile(title: Text('Item 3'))),
     ];
     List<Widget> myList2 = [
-      const ListTile(title: Text('Item 4')),
-      const ListTile(title: Text('Item 5')),
-      const ListTile(title: Text('Item 6')),
+      SizedBox(
+          width: MediaQuery.of(context).size.width / 5,
+          child: const ListTile(title: Text('Item 4'))),
+      SizedBox(
+          width: MediaQuery.of(context).size.width / 5,
+          child: const ListTile(title: Text('Item 5'))),
+      SizedBox(
+          width: MediaQuery.of(context).size.width / 5,
+          child: const ListTile(title: Text('Item 6'))),
     ];
     return Scaffold(
       appBar: AppBar(
         title: const Text("Spread Operator"),
       ),
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-          ...myList1,
-          ...myList2,
-          const ListTile(title: Text("Item 7"))
-        ],
+      body: SizedBox(
+        width: MediaQuery.sizeOf(context).width,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: [
+            ...myList1,
+            ...myList2,
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 5,
+              child: ListTile(
+                title: Text("Item 7"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
